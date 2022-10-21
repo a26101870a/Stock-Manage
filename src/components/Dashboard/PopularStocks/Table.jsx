da7be887a93810e1fcf65d09504a6ea769ace02a
a26101870a <a26101870a@gmail.com>
2022-10-21 18:15:37 +0800

    Rearrange popular stock data

--- src/components/Dashboard/PopularStocks/Table.jsx
+++ src/components/Dashboard/PopularStocks/Table.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import TableHeader from './TableHeader';
 import TableItem from './TableItem';
+import MI_INDEX20 from 'Data/MI_INDEX20.json'
 
-export default function Table({ data }) {
+export default function Table() {
     return (
         <div className="popularStocks__content">
             <table style={{ width: 100 + '%' }}>
                 <thead><TableHeader /></thead>
                 <tbody className="popularStocks__items">
-                    {data.map((item, index) => (
+                    {MI_INDEX20.map((item, index) => (
                         <TableItem data={item} key={index} />
                     ))}
                 </tbody>
--- src/components/Dashboard/PopularStocks/TableItem.jsx
+++ src/components/Dashboard/PopularStocks/TableItem.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 
 export default function TableItem({ data }) {
-    let trendSign = data[9];
+    let trendSign = data['Dir'];
     let trendClassName = 'popularItem__trend ';
 
-    if (trendSign !== 'X' && trendSign !== '') {
-        trendSign = trendSign.split('>')[1].split('<')[0];
-
-        if (trendSign === '+') {
-            trendClassName += 'trend-up ';
-        } else {
-            trendClassName += 'trend-down ';
-        }
+    if (trendSign === '+') {
+        trendClassName += 'trend-up ';
+    } else {
+        trendClassName += 'trend-down ';
     }
 
     return (
         <tr className='popularItem '>
             <td className='popularItem__title'>
-                <div className='popularItem__name'>{data[2]}</div>
-                <div className='popularItem__number'>{data[1]}</div>
+                <div className='popularItem__name'>{data['Name']}</div>
+                <div className='popularItem__number'>{data['Code']}</div>
             </td>
-            <td className='popularItem__price'>{data[8]}</td>
+            <td className='popularItem__price'>{data['ClosingPrice']}</td>
             <td className={trendClassName}>
-                {(data[9] === '' || data[9] === 'X') && '--'}
-                {(data[9] !== '' && data[9] !== 'X') && `${trendSign}${data[10]}`}
+                {trendSign}{data['Change']}
             </td>
         </tr>
     );
